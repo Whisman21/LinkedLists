3759e88eb964cff1e71d375bca874d2e367a5611
Whisman21 <144621111+Whisman21@users.noreply.github.com>
2023-09-12 10:55:12 -0400

    Update LinkedLists.cpp

--- LinkedLists.cpp
+++ LinkedLists.cpp
@@ -5,4 +5,20 @@ Fecha:
 */
 
 #include <iostream>
+#include <limits>
 using namespace std;
+
+struct LinkedList {
+ int dato;
+LinkedList *next;
+}; 
+
+int obtener_valido() {
+    int number;
+    while (!(cin >> number)) {
+        cout << "Entrada inválida: " << endl;
+        cin.clear(); // Limpia el estado de error del flujo de entrada.
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Descarta la entrada errónea.
+    }
+    return number;
+}
